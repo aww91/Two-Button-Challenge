acef1ceec63a90a44371af53cfa02a3ab868148c
Anthony Whitfield <aww91@yahoo.com>
2022-05-01 11:44:58 -0400

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Anthony Whitfield on 5/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
